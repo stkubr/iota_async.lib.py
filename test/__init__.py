7fca746b2c5bec3705988c207d2d919a570d84d2
Stanislav Kubrak <stkubr@gmail.com>
2019-04-16 23:12:22 +0200

    remove python2 fallback

--- test/__init__.py
+++ test/__init__.py
@@ -8,7 +8,3 @@ if PY3:
   # In Python 3 the ``mock`` library was moved into the stdlib.
   # noinspection PyUnresolvedReferences
   from unittest import mock
-else:
-  # In Python 2, the ``mock`` library is included as a dependency.
-  # noinspection PyUnresolvedReferences
-  import mock
